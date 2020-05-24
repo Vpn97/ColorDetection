d9335910407c42e5fc7ca11f7ce5368c2b2ffd4d
Vpn97 <vpn0097@gmail.com>
2020-05-24 12:03:31 +0530

    create util class and views

---
+++ app/src/main/java/com/apkzube/blindvision/Dashboard.java
@@ -0,0 +1,31 @@
+package com.apkzube.blindvision;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Color Detection
+ * Created by Vishal Nagvadiya on 24-05-2020.
+ */
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        allocation();
+        setEvent();
+    }
+
+    private void allocation() {
+
+    }
+
+    private void setEvent() {
+
+    }
+}
